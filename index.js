fd54b145b3c69c295eb96b1f8dd987dc90dda88f
Milamin-hub <76464674+Milamin-hub@users.noreply.github.com>
2021-07-12 19:18:36 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
-  
+const bodyParser = require('body-parser'); 
+const { request } = require("express");
 const app = express();
   
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 app.set("view engine", "pug");
+
+app.use("/public", express.static("public"))
  
 app.get("/nex", function(request, response){
       
@@ -14,7 +19,36 @@ app.get("/nex", function(request, response){
     });
 }).listen(3000); 
  
-app.get("/", function(request, response){
-      
-    response.send("Home");
+app.get("/:id", function(request, response){
+    response.send(`Товар ${request.params.id}`);
+});
+
+
+app.get("/", function(request, res){
+
+    res.render("home", {
+        title: "Home"
+    });
+
+});
+
+app.post("/nex", urlencodedParser, function(request, response){
+    if (!request.body) return res.sendStatus(404)
+    console.log(request.body)
+    response.render("log", {data: request.body});
+});
+
+
+
+
+app.get("/nex/:id", function(req, res){
+    let obj = {cor: 3443, arr: ["cern", "res", 345]}
+    res.render("nexid", {
+        title: "Мои контакты",
+        emailsVisible: true,
+        emails: ["gavgav@mycorp.com", "mioaw@mycorp.com"],
+        phone: "+1234567890",
+        ns:  req.params.id
+    });
+    console.log(req.query)
 });
